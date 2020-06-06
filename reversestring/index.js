f37bf588cc93551ee9a6e6e0129ffdff940c99d6
julius.suominen <julius.suominen@gmail.com>
2020-06-06 20:27:46 +0300

    str reverse

--- reversestring/index.js
+++ reversestring/index.js
@@ -6,6 +6,26 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+/*
+function reverse(str = '') {
+  return str.split('').reverse().join('');
+}
+*/
+
+/*
+function reverse(str = '') {
+  let reversed = '';
+  
+  for (let char of str) {
+    reversed = char + reversed;
+  }
+
+  return reversed;
+}
+*/
+
+function reverse(str = '') {
+  return str.split('').reduce((reversed, char) => char + reversed, '');
+}
 
 module.exports = reverse;
