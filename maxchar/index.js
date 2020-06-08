e514875d6070a383222b7acfa4867f9b28970702
julius.suominen <julius.suominen@gmail.com>
2020-06-08 18:07:42 +0300

    maxchar with for of, for in

--- maxchar/index.js
+++ maxchar/index.js
@@ -5,6 +5,20 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str = '') {
+  const charMap = {};
+  let max = 0;
+  let maxChar = '';
+  for (let char of str) {
+    charMap[char] = charMap[char] + 1 || 1;
+  };
+  for (let x in charMap) {
+    if (charMap[x] > max) {
+      max = charMap[x];
+      maxChar = x;
+    }
+  }
+  return maxChar;
+};
 
 module.exports = maxChar;
