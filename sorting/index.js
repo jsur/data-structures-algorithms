bbef9be9ae44ee2e8a5c1130585b1c860f5d96a1
julius.suominen <julius.suominen@gmail.com>
2020-11-01 13:32:06 +0200

    add bubble and selection sort algos

--- sorting/index.js
+++ sorting/index.js
@@ -2,15 +2,39 @@
 // Implement bubbleSort, selectionSort, and mergeSort
 
 function bubbleSort(arr) {
-
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < (arr.length - i - 1); j++) {
+      if (arr[j] > arr[j + 1]) {
+        const lesser = arr[j + 1];
+        arr[j + 1] = arr[j];
+        arr[j] = lesser;
+      }
+    }
+  }
+  return arr;
 }
 
 function selectionSort(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    let indexOfMin = i; // prove that this index is not the least value in the array
+    for (let j = i+1; j < arr.length; j++) {
+      if (arr[j] < arr[indexOfMin]) {
+        indexOfMin = j; // found a lesser value, assign that index to indexOfMin
+      }
+    }
 
+    if (indexOfMin !== i) {
+      let lesser = arr[indexOfMin];
+      arr[indexOfMin] = arr[i];
+      arr[i] = lesser;
+    }
+  }
+
+  return arr;
 }
 
 function mergeSort(arr) {
-
+  
 }
 
 function merge(left, right) {
